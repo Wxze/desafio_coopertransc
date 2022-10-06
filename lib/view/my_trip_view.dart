c2385b4f424275865d8dd449b07ce2fe6ab6e8cc
Ranniery Moreira <moreiraranni@gmail.com>
2022-10-05 21:29:45 -0300

    Add warning card and list to project

--- lib/view/my_trip_view.dart
+++ lib/view/my_trip_view.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 import '../widgets/trip_list_card.dart';
---
+++ lib/widgets/warning_list_card.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class WarningListCard extends StatelessWidget {
+  final String title;
+  final String message;
+  const WarningListCard(this.title, this.message, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        elevation: 3,
+        child: ClipPath(
+          clipper: ShapeBorderClipper(
+              shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5),
+          )),
+          child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              decoration: BoxDecoration(
+                border: Border(
+                    left: BorderSide(
+                        color: const Color(0xFF1A5650).withOpacity(0.75),
+                        width: 5)),
+                color: Colors.white,
+              ),
+              child: Expanded(
+                  child: Column(
+                children: [
+                  Row(
+                    children: [
+                      const Icon(Icons.warning, color: Color(0xFF1A5650)),
+                      const SizedBox(width: 16),
+                      Text(title,
+                          style: const TextStyle(
+                            color: Color(0xFF000D0C),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ))
+                    ],
+                  ),
+                  const SizedBox(height: 12),
+                  Column(children: [
+                    Text(message,
+                        textAlign: TextAlign.justify,
+                        style: const TextStyle(
+                            color: Color(0xFF000D0C), fontSize: 14))
+                  ])
+                ],
+              ))),
+        ));
+  }
+}
