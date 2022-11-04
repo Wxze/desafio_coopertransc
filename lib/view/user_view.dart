f79187a729b31642605d38b4aa5d7914832b653c
Gabriel Jose <gabrieljose528@gmail.com>
2022-11-04 20:53:18 -0300

    Add edit user screen

--- lib/view/user_view.dart
+++ lib/view/user_view.dart
@@ -1,3 +1,5 @@
+import 'package:desafio_coopertransc/widgets/default_button.dart';
+import 'package:desafio_coopertransc/widgets/default_text_field.dart';
 import 'package:flutter/material.dart';
 
 class UserView extends StatefulWidget {
@@ -8,15 +10,121 @@ class UserView extends StatefulWidget {
 }
 
 class _UserViewState extends State<UserView> {
+  var txtUser = TextEditingController();
+  var txtEmail = TextEditingController();
+  var txtPassword = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        child: Center(child: Text('teste'))
+    return Scaffold(
+      backgroundColor: const Color(0xFF1A5650),
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+        elevation: 0,
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: handleClick,
+            itemBuilder: (context) {
+              return {'Sair'}.map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(choice),
+                );
+              }).toList();
+            },
+          )
+        ],
       ),
-      
+      body: Card(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25), topRight: Radius.circular(25))),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundColor: Colors.grey.shade500,
+                ),
+                const SizedBox(
+                  width: 45,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Gabriel Toledo',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 30),
+                    ),
+                    Text(
+                      'presenteprofessor@gmail.com',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w400, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            const Center(
+              child: Text(
+                'Editar usuário',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+              ),
+            ),
+            Flexible(
+              fit: FlexFit.tight,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                child: Form(
+                  child: Column(
+                    children: [
+                      DefaultTextField(
+                        controllerVariable: txtUser,
+                        label: 'Usuário',
+                        isPassword: false,
+                        icon: Icons.person,
+                      ),
+                      DefaultTextField(
+                        controllerVariable: txtEmail,
+                        label: 'Email',
+                        isPassword: false,
+                        icon: Icons.email,
+                      ),
+                      DefaultTextField(
+                        controllerVariable: txtPassword,
+                        label: 'Senha',
+                        isPassword: true,
+                        icon: Icons.key,
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      DefaultButton(label: 'Editar', onClick: () {}),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void handleClick(String value) {
+    switch (value) {
+      case 'Sair':
+        Navigator.pushNamed(context, '/login');
+        break;
+    }
   }
 }
